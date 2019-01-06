e80b9240b8af499a0c9633bcc769b271492e9b61
Aviv Segal <avivsegal@gmail.com>
2019-01-06 23:11:12 +0200

    Added tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,24 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 class UserController {
+    static getToken(user) {
+        // TODO: Edit the secret with local variable.
+        return { token: jwt.sign(
+            {
+                email: user.email,
+                userId: user._id
+            },
+            'todo_edit_this_secret',
+            {
+                expiresIn: "1h"
+            }).cath(error => {
+                console.log(error);
+            })
+        };
+    };
+
     static async getUserCollection(body) {
         let result = {status: 200, data: null};
         // TODO: error handler
@@ -12,47 +29,40 @@ class UserController {
                 result.data = err;
             }
         });
-        //console.log(result);
         return result;
     };
 
     static async createUser(body) {
-        let result = null;
+        let error = false;
+        let user = await User.findOne({email: body.email});
+        if (user) {
+            return {error: true, details: "email address already exists."};
+        }
         // TODO: Check body schema
         body.password = await bcrypt.hash(body.password, await bcrypt.genSalt(10));
 
-        const user = new User(body);
+        user = new User(body);
 
         await user.save(err => {
             if (err) {
-                result = err;
-            } else {
-                // TODO add token!!!
-                result = "token";
+                error = true;
             }
         });
-        return result;
+
+        return error?{error: true, details: "cannot save to db"}:this.getToken(user);
     };
 
     static async login(body) {
         let result = null;
-        const invalid = "Invalid email or password.";
         let user = await User.findOne({email: body.email});
-        if (!user) {
-            result = invalid;
-        }
-        else if (body.password === user.password) {
-            // TODO add token!!!
-            return "token";
-        }
-        else
-        {
-            result = invalid;
+
+        if (user && await bcrypt.compare(body.password, user.password)) {
+            return this.getToken(user);
+        } else {
+            result = "Invalid email or password.";
         }
 
         return result;
-
-
     };
 
     static async getUser(email) {
