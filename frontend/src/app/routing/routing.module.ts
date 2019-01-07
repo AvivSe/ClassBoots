a2d1673771bc68ece5ad833fd6c6ceabee1bb4d0
Aviv Segal <avivsegal@gmail.com>
2019-01-07 12:57:02 +0200

    Iluz frontend

--- frontend/src/app/routing/routing.module.ts
+++ frontend/src/app/routing/routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from "@angular/router";
 import { HomeComponent } from "../pages/home/home.component";
-import {VideoComponent} from "../pages/video/video.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'video', component: VideoComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
