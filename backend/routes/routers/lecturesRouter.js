77c69473cd1b62678d6cc47397a8fa90a5fc9ed2
Aviv Segal <avivsegal@gmail.com>
2019-01-07 12:36:27 +0200

    Deleting local decencies

--- backend/routes/routers/lecturesRouter.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var defineRoutes = require('../routes/lecturesRoutes');
-
-var lecturesRouter = express.Router();
-
-module.exports = defineRoutes(lecturesRouter);
--- backend/routes/routers/videosRouter.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var defineRoutes = require('../routes/videosRoutes');
-
-var videosRouter = express.Router();
-
-module.exports = defineRoutes(videosRouter);
