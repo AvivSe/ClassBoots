b3fc43a4b6382114a533bc151a352984d25dbb62
Aviv Segal <avivsegal@gmail.com>
2019-02-03 21:16:03 +0200

    User profile added to tokens

--- backend/utils/Token.js
+++ backend/utils/Token.js
@@ -7,6 +7,7 @@ class Token {
     constructor(user) {
         user.password = "******";
         this._token = jwt.sign({user:user}, 'todo_edit_this_secret', {expiresIn: "1h"});
+        this._profile = user;
     }
 }
 
