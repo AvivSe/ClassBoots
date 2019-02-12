d7a7b7b50bfcd582a6a159e47121413f27f53e30
galkeidarrr <39056499+galkeidarrr@users.noreply.github.com>
2019-02-12 21:04:43 +0200

    scraping

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -13,7 +13,7 @@ class VideoController {
         result = await Video.find(err => {
             if (err) {
                 result = invalid;
-                errorsController.logger(err,result);
+                errorsController.logger(err, result);
 
             }
         });
@@ -24,16 +24,17 @@ class VideoController {
         const video = new Video(body);
         //TODO: duplicate code
 
-        YoutubeCommentsScraper.getCommentsAsync(body.reference,result=>{
+        YoutubeCommentsScraper.getCommentsAsync(body.reference, result => {
             Video.findByIdAndUpdate(
                 video._id,
-                { $push: {"ytcomment": result} },
-                { upsert: true, new: true }).then(x=>{});
+                {$push: {"ytcomment": result}},
+                {upsert: true, new: true}).then(x => {
+            });
         });
         console.log("gal");
         await video.save(err => {
             if (err) {
-                errorsController.logger("Create Video",err);
+                errorsController.logger("Create Video", err);
             }
         });
         return video;
@@ -44,41 +45,39 @@ class VideoController {
         await Video.findOne({_id: id}).then(video => {
             if (video) {
                 result = video;
-                let secondesAgo=(new Date()-video.lastscrape)/1000;
-                if(secondesAgo>60){
+                let secondesAgo = (new Date() - video.lastscrape) / 1000;
+                if (secondesAgo > 60) {
                     Video.findByIdAndUpdate(
                         video._id,
-                        { lastscrape: new Date()},
-                        { upsert: true, new: true }).then(vid=>{
+                        {lastscrape: new Date()},
+                        {upsert: true, new: true}).then(vid => {
                         //TODO: duplicate code
-                        YoutubeCommentsScraper.getCommentsAsync(vid.reference,result=>{
-                            if(!vid.ytcomment.find(c=>{
-                                return (c.content===result.content && c.author===result.author);
-                            })){
+                        YoutubeCommentsScraper.getCommentsAsync(vid.reference, result => {
+                            if (!vid.ytcomment.find(c => {
+                                return (c.content === result.content && c.author === result.author);
+                            })) {
                                 Video.findByIdAndUpdate(
                                     vid._id,
-                                    { $push: {"ytcomment": result},lastscrape: new Date() },
-                                    { upsert: true, new: true }).then(ignore=>{});
-                            }
-                            else{
+                                    {$push: {"ytcomment": result}, lastscrape: new Date()},
+                                    {upsert: true, new: true}).then(ignore => {
+                                });
+                            } else {
                                 console.log("already exist");
                             }
 
                         });
                     });
 
-                }
-                else{
+                } else {
                     console.log("dont wanna update because so young :" + secondesAgo);
 
                 }
 
-            }
-            else
-                result = {"ERROR":"video not found"};
+            } else
+                result = {"ERROR": "video not found"};
         }).catch(err => {
-            result = {"ERROR":"video not found"};
-            errorsController.logger("Get Subject",err);
+            result = {"ERROR": "video not found"};
+            errorsController.logger("Get Subject", err);
         });
         return result;
     };
@@ -87,14 +86,14 @@ class VideoController {
         let result = null;
         await Video.findByIdAndDelete(id).catch(err => {
             result = err;
-            errorsController.logger(err,result);
+            errorsController.logger(err, result);
         });
         return result;
     };
 
     static async updateVideo(body) {
         let result = await Video.findByIdAndUpdate(body._id, body, {new: true}, err => {
-            if (err) errorsController.logger("update Video",err);
+            if (err) errorsController.logger("update Video", err);
         });
         return result;
     }
@@ -102,25 +101,28 @@ class VideoController {
     static async addComment(body) {
         var result = await Video.findByIdAndUpdate(
             body.videoid,
-            { $push: {"comments": body}},
-            { upsert: true, new: true });
+            {$push: {"comments": body}},
+            {upsert: true, new: true});
         return result;
     };
 
     static async deleteComment(body) {
         var result = await Video.findByIdAndUpdate(
             body.videoid,
-            { $pull: {"comments": {  _id: body.commentid }}},
-            { upsert: true, new: true });
+            {$pull: {"comments": {_id: body.commentid}}},
+            {upsert: true, new: true});
         return result;
     };
 
     static async checkPermission(body) {
         var result = await this.getVideo(body.videoid);
         console.log(LectureController.getAviv());
-        return await result.ERROR === undefined?LectureController.checkPermission({lectureid:result.lectureid,userid:body.userid}).finally(()=>{}):false;
+        return await result.ERROR === undefined ? LectureController.checkPermission({
+            lectureid: result.lectureid,
+            userid: body.userid
+        }).finally(() => {
+        }) : false;
     };
-
 
 
 }
