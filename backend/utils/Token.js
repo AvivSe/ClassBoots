ba17d7914f418fa932147bc71975081c44c01b1b
iluzamit <34029258+iluzamit@users.noreply.github.com>
2019-02-24 12:02:23 +0200

    unready.

--- backend/utils/Token.js
+++ backend/utils/Token.js
@@ -1,8 +1,4 @@
 const jwt = require('jsonwebtoken');
-
-/**
- * Created by Aviv Segal on Jan 2019
- */
 class Token {
     constructor(user) {
         this._token = jwt.sign({user:user}, 'todo_edit_this_secret', {expiresIn: "1h"});
