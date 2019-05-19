51b856bb5b78d0358bf7a4aaa70ead4e066eae11
Nir-Elk <nirelkayam31@gmail.com>
2019-05-19 16:24:25 +0300

    add support to permission

--- backend/utils/Role.js
+++ backend/utils/Role.js
@@ -34,7 +34,7 @@ class Role {
     }
     static admin(req, res, next) {
         if(req.profile.user.role && req.profile.user.role==='admin') {
-            console.log('admin');
+//            console.log('admin');
             next();
         } else {
             res.status(405).send({error:true,description:'Auth-Failed: only admin can access'});
