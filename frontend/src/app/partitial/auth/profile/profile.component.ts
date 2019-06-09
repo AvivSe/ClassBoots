7a472dbc2d8eb23ad8be16b6a42210f7a5c1c7fe
Amit Iluz <iluzzamit@gmail.com>
2019-06-09 20:12:17 +0300

    hello world.

--- frontend/src/app/partitial/auth/profile/profile.component.ts
+++ frontend/src/app/partitial/auth/profile/profile.component.ts
@@ -5,45 +5,65 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  user : userData = null;
-  constructor(@Inject(MAT_DIALOG_DATA) public data, public http: HttpClient) {
-    this.http.get<{profile: userData, error : boolean}>(environment.baseUrl + "api/user/profile")
-        .subscribe(user =>{
-          if(!user.error) {
-            this.user = user.profile;
-          }
-        });
-  }
+    user: userData;
+    error: boolean = false;
+    errorMsg: string;
+    isLoaded: boolean = false;
 
-  updateUser(userForm){
-      if(userForm.value.firstName && userForm.value.firstName != ''){
-          this.user.firstName = userForm.value.firstName;
-      }
-      if(userForm.value.lastName && userForm.value.lastName != ''){
-          this.user.lastName = userForm.value.lastName;
-      }
-      if(userForm.value.dob && userForm.value.dob != ''){
-          this.user.dob = userForm.value.dob;
-      }
-      this.http.post<{profile: userData, error : boolean}>(environment.baseUrl + "api/user/profile",{
-          firstName: this.user.firstName,lastName: this.user.lastName,dob:this.user.dob
-      })
-          .subscribe(user =>{
-              if(!user.error) {
-                  this.user = user.profile;
-              }
-          });
-      userForm.reset();
-  }
 
-  updatePassword(passwordForm){
-      console.log(passwordForm.value)
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) public data, public http: HttpClient) {
+        this.http.get<{ profile: userData, error: boolean }>(environment.baseUrl + "api/user/profile")
+            .subscribe(user => {
+                if (!user.error) {
+                    this.user = user.profile;
+                    this.isLoaded = true;
+                }
+            });
+    }
+
+    updateUser(userForm) {
+        if (userForm.value.firstName && userForm.value.firstName != '') {
+            this.user.firstName = userForm.value.firstName;
+        }
+        if (userForm.value.lastName && userForm.value.lastName != '') {
+            this.user.lastName = userForm.value.lastName;
+        }
+        if (userForm.value.dob && userForm.value.dob != '') {
+            this.user.dob = userForm.value.dob;
+        }
+        this.http.put<{ profile: userData, error: boolean, description: string }>(environment.baseUrl + "api/user/profile", {
+            firstName: this.user.firstName, lastName: this.user.lastName, dob: this.user.dob
+        })
+            .subscribe(user => {
+                if (!user.error) {
+                    userForm.reset();
+                } else {
+                    this.error = true;
+                    this.errorMsg = user.description
+                }
+            });
+    }
+
+    updatePassword(passwordForm) {
+        if (passwordForm.value.password == passwordForm.value.repassword) {
+            this.http.put<{ profile: userData, error: boolean }>(environment.baseUrl + "api/user/profile", {
+                password: passwordForm.value.password
+            })
+                .subscribe(user => {
+                    if (!user.error) {
+                        passwordForm.reset();
+                        this.error = false;
+                    }
+                })
+        } else {
+            this.error = true; this.errorMsg = "New password doesnt match!"
+        }
+    }
 }
 
 
